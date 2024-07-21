191baebd372678bad49ce63f0f334cea4bc19ebe
Zakiaghulamhussain <156411986+Zakiaghulamhussain@users.noreply.github.com>
2024-07-21 17:01:58 +0500

    smit practice test

---
+++ app.js
@@ -0,0 +1,31 @@
+// //Question#1
+//     var myArray=["cat","dog","rabbit","hen","peacock","goat"];
+//     console.log(myArray.reverse());
+
+// //Question#2
+// var myArray = ["1", "2", "-2","-4","-3",];
+// console.log(myArray);
+// let newArray=myArray.slice(2)
+// console.log(newArray);
+
+
+// //Question#6
+// let arr = [1, 2, 3, 4];
+// let mapped_arr = arr.map(x => x + 1);
+// console.log(mapped_arr);
+
+// //Question#7
+// var name=("zakia","sara","zara","aliya","noor");
+// var updatedValue=name.slice(3,5)*4
+// console.log(updatedValue);
+
+//Question#10
+var array=["dog","cat","rabbit","hen"];
+for (let i=0;i<array.length;i++){
+    console.log(i);
+}
+
+
+// //Question#3
+// var num=("a","e","i","o","u")
+
